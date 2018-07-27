35cd75e5dbec178186da359692ae144438f6f465
Max <maxtucker2191@gmail.com>
2018-07-27 21:41:51 +0100

    profile route seperated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require('knex')
 
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
 
 
 const db = knex({
@@ -33,20 +34,9 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) })
 
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
