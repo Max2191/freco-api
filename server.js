c9f6d25c74d9b6a2bde1f315495af094f464b09a
Max <maxtucker2191@gmail.com>
2018-07-27 21:55:04 +0100

    minor change

--- server.js
+++ server.js
@@ -25,19 +25,11 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {
-  res.send(db.users);
-})
 
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
-
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) })
-
-
 app.put('/image', (req, res) => {image.handleImage(req, res, db) })
 
 
